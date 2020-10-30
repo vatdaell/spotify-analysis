fb8082504792abbc97132ca3af4df5986f9a0dc1
Ansaf Ahmad <ansafahmad70@gmail.com>
2020-10-30 03:30:35 -0400

    Added a way to load recently played data to sql db

---
+++ src/etl/recently_played_load.py
@@ -0,0 +1,35 @@
+from common.db import DB
+from common.store import Store
+from common.timehelpers import extractDate
+from dotenv import load_dotenv, find_dotenv
+from os import getenv
+from io import StringIO
+import pandas as pd
+
+PREFIX = "weekly_reports"
+
+if __name__ == "__main__":
+    load_dotenv(find_dotenv())
+    HOST = getenv("MYSQL_HOST")
+    PORT = getenv("MYSQL_PORT")
+    USER = getenv("MYSQL_USER")
+    PASS = getenv("MYSQL_PASS")
+    DATABASE = getenv("MYSQL_DB")
+
+    db = DB(HOST, PORT, USER, PASS, DATABASE)
+    store = Store(getenv("S3_BUCKET"))
+
+    files = store.getFiles(PREFIX)
+    file_names = list(filter(lambda x: ".csv" in x, map(lambda x: x.key, files)))
+
+    latest_date = max(list(map(lambda x: extractDate(x, PREFIX, ".csv"), file_names)))
+    latest_file = "{}/{}.{}".format(PREFIX, latest_date, "csv")
+
+    # Get File
+    body = store.getFile(latest_file)
+    csv = pd.read_csv(StringIO(body), low_memory=False)
+    csv = csv[['artist', 'album', 'track', 'duration', 'popularity', 'played_at', 'explicit']]
+    csv_tuple = [tuple(x) for x in csv.to_numpy()]
+
+    # Load data to sql
+    db.insertRecentPlays(csv_tuple)
